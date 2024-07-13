727bd04c8fca8968f42253846506a472e8030cf8
Isshi0417 <isshi0417@gmail.com>
2024-07-13 23:27:28 +0900

    Configure grub theme

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
     # Bootloader.
@@ -11,6 +11,15 @@
             efiSupport = true;
             device = "nodev";
             useOSProber = true;
+
+            theme = "${
+                (pkgs.fetchFromGitHub {
+                    owner = "AllJavi";
+                    repo = "tartarus-grub";
+                    rev = "b116360a2a0991062a4d728cb005dfd309fbb82a";
+                    hash = "sha256-/Pzr0R3zzOXUi2pAl8Lvg6aHTiwXTIrxQ1vscbEK/kU=";
+                })
+            }/tartarus/";
         };
     };
 }
