625b246da33dc22dc7c40b7b15700039f22cc33a
Isshi0417 <isshi0417@gmail.com>
2024-07-13 23:31:55 +0900

    Improve code

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -12,14 +12,20 @@
             device = "nodev";
             useOSProber = true;
 
-            theme = "${
-                (pkgs.fetchFromGitHub {
+            theme = pkgs.stdenv.mkDerivation {
+                name = "tartarus-grub";
+                
+                src = pkgs.fetchFromGitHub {
                     owner = "AllJavi";
                     repo = "tartarus-grub";
                     rev = "b116360a2a0991062a4d728cb005dfd309fbb82a";
                     hash = "sha256-/Pzr0R3zzOXUi2pAl8Lvg6aHTiwXTIrxQ1vscbEK/kU=";
-                })
-            }/tartarus/";
+                };
+
+                installPhase = ''
+                    cp tartarus -r $out
+                '';
+            };
         };
     };
 }
