19bc174c77efea2e0d5b2fd99d58bd010bf32908
Isshi0417 <isshi0417@gmail.com>
2024-07-15 22:58:15 +0900

    Change wezterm font

--- modules/home-manager/terminal/wezterm/config/wezterm.lua
+++ modules/home-manager/terminal/wezterm/config/wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.font = wezterm.font 'GohuFont 14 Nerd Font Mono'
 
 return config
