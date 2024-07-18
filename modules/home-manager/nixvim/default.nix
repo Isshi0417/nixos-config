808fc11662412fb1c83e1c7ee2fe69a6b3e68794
Isshi0417 <isshi0417@gmail.com>
2024-07-19 06:52:18 +0900

    Configure nixvim

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -5,5 +5,23 @@
     enable = true;
     defaultEditor = true;
     vimdiffAlias = true;
+    
+    enableMan = true;
+
+    colorschemes.gruvbox.enable = true;
+    
+    plugins = {
+      lualine.enable = true;
+      treesitter.enable = true;
+      telescope.enable = true;
+      lazygit.enable = true;
+      cmp.enable = true;
+      sleuth.enable = true;
+      nvim-autopairs.enable = true;
+      neoscroll.enable = true;
+      indent-blankline.enable = true;
+      bufferline.enable = true;
+      noice.enable = true;
+    };  
   };
 }
